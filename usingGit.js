c9b6bdb90388f850b0487d849277afabb61c60fa
Natnael <natibir400@gamil.com>
2023-12-17 19:12:44 +0300

    changed directory

--- usingGit.js
+++ usingGit.js
@@ -1,3 +1,31 @@
-console.log("trying out git")
-console.log("first commit from vscode")
-console.log("changes made")
+var divide = function(dividend, divisor) {
+    let counter = 0;
+    let Tdividend = Math.abs(dividend);
+    let Tdivisor = Math.abs(divisor);
+
+    if (divisor == 1) {
+        counter = dividend;
+    } else if (divisor == -1) {
+        counter = -dividend;
+    } else {
+        for (let i = 30; i >= 0; i--) {
+            if (Tdivisor << i <= Tdividend) {
+                Tdividend -= Tdivisor << i;
+             //   console.log(Tdividend)
+                counter += 1 << i;
+                console.log(counter)
+            }
+        }
+
+      //  (divisor < 0 && dividend >= 0) || (divisor >= 0 && dividend < 0) ? (counter = -counter) : counter;
+    }
+
+    if (counter > (2**31) - 1) {
+        return (2**31) - 1;
+    } else if (counter < (-2)**31) {
+        return (-2)**31;
+    } else {
+        return counter;
+    }
+};
+console.log(divide(10,3))
